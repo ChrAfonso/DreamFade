f662921ec81a99c864e6753ce244cf31b8e2d589
Christian Afonso <chr.afonso@gmail.com>
2016-08-07 02:19:09 +0200

    Added CameraBob to Player prefab to simulate spirit floating...

---
+++ Assets/Scripts/CameraBob.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBob : MonoBehaviour {
+
+	private float originalY;
+
+	public float SpeedY = 0.8f;
+	public float SpeedX = 1f;
+	public float BobHeight = 0.5f;
+	public float BobWidth = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+		originalY = transform.localPosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.localPosition;
+		pos.y = originalY + Mathf.Sin(Time.time * SpeedY) * BobHeight;
+		pos.x = Mathf.Sin(Time.time * SpeedX) * BobWidth;
+		transform.localPosition = pos;
+	}
+}
